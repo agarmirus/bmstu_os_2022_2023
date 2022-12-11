0cb53b8489ffa468310a23ab8243493489a29560
agarmirus <tsarevantuan@yandex.ru>
2022-12-11 23:51:46 +0300

    Written pc prog

--- lab_04/pc/main.c
+++ lab_04/pc/main.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,12 +8,195 @@
 #include <sys/ipc.h>
 #include <sys/wait.h>
 
-/*
- * Никаких массивов не должно быть. Работать только с указателями
- * В инете есть вариант, где задается строка букв латинского алфавита.
- * Достаточно установить значение переменной и инкрементировать ее.
- * Никакого контроля записанных символов делать не надо: и так есть семафоры.
- */
+#define EMPTY_CELLS_SEM 0
+#define FILED_CELLS_SEM 1
+#define BIN_SEM 2
+
+#define BUFFER_ELEMS_COUNT 15
+
+#define CHILDREN_COUNT 6
+#define PRODUCERS_COUNT 3
+
+typedef struct
+{
+    char *cptr;     // Указатель для потребителя
+    char *pptr;     // Указатель для производителя
+    char letter;
+} buffer_t;
+
+static int start_produce(const int semid)
+{
+    struct sembuf sops[] = {
+        {EMPTY_CELLS_SEM, -1, 0},
+        {BIN_SEM, -1, 0}
+    };
+    return semop(semid, sops, 2);
+}
+
+static int stop_produce(const int semid)
+{
+    struct sembuf sops[] = {
+        {BIN_SEM, 1, 0},
+        {FILED_CELLS_SEM, 1, 0}
+    };
+    return semop(semid, sops, 2);
+}
+
+static int start_consume(const int semid)
+{
+    struct sembuf sops[] = {
+        {FILED_CELLS_SEM, -1, 0},
+        {BIN_SEM, -1, 0}
+    };
+    return semop(semid, sops, 2);
+}
+
+static int stop_consume(int semid)
+{
+    struct sembuf sops[] = {
+        {BIN_SEM, 1, 0},
+        {EMPTY_CELLS_SEM, 1, 0}
+    };
+    return semop(semid, sops, 2);
+}
+
+static int run_producer(buffer_t *shmptr, const int semid)
+{
+    srand(getpid());
+    size_t producings_count = 5;
+    for (size_t i = 0; i < producings_count; ++i)
+    {
+        sleep(rand() % 4);
+        if (start_produce(semid) == -1)
+        {
+            perror("Cannot start producer");
+            exit(1);
+        }
+        *shmptr->pptr = shmptr->letter;
+        printf("Producer (PID %d) wrote: %c\n", getpid(), shmptr->letter);
+        ++shmptr->letter;
+        ++shmptr->pptr;
+        if (stop_produce(semid) == -1)
+        {
+            perror("Cannot stop producer");
+            exit(1);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+static int run_consumer(buffer_t *shmptr, const int semid)
+{
+    srand(getpid());
+    size_t consumings_count = 5;
+    for (size_t i = 0; i < consumings_count; ++i)
+    {
+        sleep(rand() % 4);
+        if (start_consume(semid) == -1)
+        {
+            perror("Cannot start producer");
+            exit(1);
+        }
+        printf("Consumer (PID %d) read: %c\n", getpid(), *shmptr->cptr);
+        ++shmptr->cptr;
+        if (stop_consume(semid) == -1)
+        {
+            perror("Cannot stop producer");
+            exit(1);
+        }
+    }
+    return EXIT_SUCCESS;
+}
 
 int main(void)
-{}
+{
+    key_t key = ftok("/dev/null", IPC_PRIVATE);
+    int perms = S_IRWXU | S_IRWXG | S_IRWXO;
+    int shmid = shmget(key, BUFFER_ELEMS_COUNT * sizeof(char) + sizeof(buffer_t), perms | IPC_CREAT);
+    if (shmid == -1)
+    {
+        perror("Cannot create shared memory");
+        exit(1);
+    }
+    buffer_t *shmptr = shmat(shmid, NULL, 0);
+    if (shmptr == (void *)-1)
+    {
+        perror("Cannot get shared memory address");
+        exit(1);
+    }
+    shmptr->cptr = shmptr->pptr = (char *)shmptr + sizeof(buffer_t);
+    shmptr->letter = 'a';
+    int semid = semget(key, 3, perms | IPC_CREAT);
+    if (semid == -1)
+    {
+        perror("Cannot create semaphores set");
+        exit(1);
+    }
+    semctl(semid, EMPTY_CELLS_SEM, SETVAL, BUFFER_ELEMS_COUNT);
+    semctl(semid, FILED_CELLS_SEM, SETVAL, 0);
+    semctl(semid, BIN_SEM, SETVAL, 1);
+    pid_t child_pids[CHILDREN_COUNT];
+    for (size_t i = 0; i < PRODUCERS_COUNT; ++i)
+    {
+        if ((child_pids[i] = fork()) == -1)
+        {
+            perror("Cannot fork");
+            exit(1);
+        }
+        else if (child_pids[i] == 0)
+        {
+            int rc = run_producer(shmptr, semid);
+            if (rc != EXIT_SUCCESS)
+            {
+                perror("Reader error");
+                exit(1);
+            }
+            return EXIT_SUCCESS;
+        }
+    }
+    for (size_t i = PRODUCERS_COUNT; i < CHILDREN_COUNT; ++i)
+    {
+        if ((child_pids[i] = fork()) == -1)
+        {
+            perror("Cannot fork");
+            exit(1);
+        }
+        else if (child_pids[i] == 0)
+        {
+            int rc = run_consumer(shmptr, semid);
+            if (rc != EXIT_SUCCESS)
+            {
+                perror("Writer error");
+                exit(1);
+            }
+            return EXIT_SUCCESS;
+        }
+    }
+    for (size_t i = 0; i < CHILDREN_COUNT; ++i)
+    {
+        int status;
+        if (waitpid(child_pids[i], &status, 0) == -1)
+        {
+            perror("waitpid error");
+            exit(1);
+        }
+        if (!WIFEXITED(status))
+            printf("Child process (PID %d) crashed\n", child_pids[i]);
+    }
+    if (shmdt(shmptr) == -1)
+    {
+        perror("shmdt error");
+        exit(1);
+    }
+    if (shmctl(shmid, IPC_RMID, NULL) == -1)
+    {
+        perror("shmctl error");
+        exit(1);
+    }
+    if (semctl(semid, 0, IPC_RMID) == -1)
+    {
+        perror("semctl error");
+        exit(1);
+    }
+    return EXIT_SUCCESS;
+}
--- lab_04/wr/main.c
+++ lab_04/wr/main.c
@@ -7,95 +7,205 @@
 #include <sys/ipc.h>
 #include <sys/wait.h>
 
-/*
- * Использовать функцию ftok
- */
+#define READERS_COUNT_SEM 0     // Количество читателей в очереди
+#define WRITERS_COUNT_SEM 1     // Количество писателей в очереди
+#define IS_READING_SEM 2        // Активный читатель
+#define IS_WRITING_SEM 3        // Активный писатель
+#define BIN_CR 4                // Захват критической секции
 
 #define CHILDREN_COUNT 7
 #define WRITERS_COUNT 3
 
-static int reader_action(const int shmid, const int semsid)
-{}
+static int start_read(const int semid)
+{
+    struct sembuf sops[] = {
+        {READERS_COUNT_SEM, 1, 0},
+        {WRITERS_COUNT_SEM, 0, 0},
+        {IS_WRITING_SEM, 0, 0},
+        {IS_READING_SEM, 1, 0}
+    };
+    return semop(semid, sops, 4);
+}
 
-static int writer_action(const int shmid, const int semsid)
-{}
+static int stop_read(const int semid)
+{
+    struct sembuf sops[] = {
+        {READERS_COUNT_SEM, -1, 0},
+        {IS_READING_SEM, -1, 0}
+    };
+    return semop(semid, sops, 2);
+}
+
+static int start_write(const int semid)
+{
+    struct sembuf sops[] = {
+        {WRITERS_COUNT_SEM, 1, 0},
+        {READERS_COUNT_SEM, 0, 0},
+        {IS_WRITING_SEM, 0, 0},
+        {BIN_CR, -1, 0},
+        {IS_WRITING_SEM, 1, 0}
+    };
+    return semop(semid, sops, 5);
+}
+
+static int stop_write(int semid)
+{
+    struct sembuf sops[] = {
+        {WRITERS_COUNT_SEM, -1, 0},
+        {IS_WRITING_SEM, -1, 0},
+        {BIN_CR, 1, 0}
+    };
+    return semop(semid, sops, 3);
+}
+
+static int run_reader(void *shmptr, const int semid)
+{
+    if (!shmptr)
+    {
+        perror("Invalid shared memory pointer");
+        exit(1);
+    }
+    size_t readings_count = 3; 
+    for (size_t i = 0; i < readings_count; ++i)
+    {
+        if (start_read(semid) == -1)
+        {
+            perror("Cannot start reading");
+            exit(1);
+        }
+        int value = *(int *)shmptr;
+        printf("Reader (PID %d) read: %d", getpid(), value);
+        if (stop_read(semid) == -1)
+        {
+            perror("Cannot stop reading");
+            exit(1);
+        }
+        sleep(rand() % 4);
+    }
+    return EXIT_SUCCESS;
+}
+
+static int run_writer(void *shmptr, const int semid)
+{
+    if (!shmptr)
+    {
+        perror("Invalid shared memory pointer");
+        exit(1);
+    }
+    size_t writings_count = 3;
+    for (size_t i = 0; i < writings_count; ++i)
+    {
+        if (start_write(semid) == -1)
+        {
+            perror("Cannot start writing");
+            exit(1);
+        }
+        int new_value = ++(*(int*)shmptr);
+        printf("Writer (PID %d) written: %d", getpid(), new_value);
+        if (stop_write(semid) == -1)
+        {
+            perror("Cannot stop writing");
+            exit(1);
+        }
+        sleep(rand() % 4);
+    }
+    return EXIT_SUCCESS;
+}
 
 int main(void)
 {
-    /*
-     * S_IRWXU - пользователь имеет право на чтение, запись и выполнение файла
-     * S_IRWXG - группа имеет право на чтение, запись и выполнение файла
-     * S_IRWXO - остальные имеют право на чтение, запись и выполнение файла
-     */
+    key_t key = ftok("/dev/null", IPC_PRIVATE);
     int perms = S_IRWXU | S_IRWXG | S_IRWXO;
-    // Создаем сегмент разделяемой памяти
-    int shmid = shmget(100, 1024, O_CREAT | perms);
+    int shmid = shmget(key, sizeof(int), O_CREAT | perms);
     if (shmid == -1)
     {
         perror("Cannot create shared memory");
         exit(1);
     }
-    // Создаем набор семафоров
-    int semsid = semget(100, 1, IPC_CREAT | perms);
-    if (semsid == -1)
+    void *shmptr = shmat(shmid, NULL, 0);
+    if (shmptr == (void *)-1)
     {
-        perror("Cannot create semaphore set");
+        perror("Cannot get shared memory address");
         exit(1);
     }
-    // Создаем писателей
+    int semid = semget(key, 5, IPC_CREAT | perms);
+    if (semid == -1)
+    {
+        perror("Cannot create semaphores set");
+        exit(1);
+    }
+    semctl(semid, IS_READING_SEM, SETVAL, 0);
+    semctl(semid, IS_WRITING_SEM, SETVAL, 0);
+    semctl(semid, READERS_COUNT_SEM, SETVAL, 0);
+    semctl(semid, WRITERS_COUNT_SEM, SETVAL, 0);
+    semctl(semid, BIN_CR, SETVAL, 0);
     pid_t child_pids[CHILDREN_COUNT];
     for (size_t i = 0; i < WRITERS_COUNT; ++i)
     {
         if ((child_pids[i] = fork()) == -1)
         {
-            perror("Cannot fork\n");
+            perror("Cannot fork");
             exit(1);
         }
         else if (child_pids[i] == 0)
         {
-            int rc = reader_action(shmid, semsid);
+            int rc = run_reader(shmptr, semid);
             if (rc != EXIT_SUCCESS)
             {
-                perror("Cannot read from shared memory");
+                perror("Reader error");
                 exit(1);
             }
             return EXIT_SUCCESS;
         }
     }
-    // Создаем читателей
     for (size_t i = WRITERS_COUNT; i < CHILDREN_COUNT; ++i)
     {
         if ((child_pids[i] = fork()) == -1)
         {
-            perror("Cannot fork\n");
+            perror("Cannot fork");
             exit(1);
         }
         else if (child_pids[i] == 0)
         {
-            int rc = writer_action(shmid, semsid);
+            int rc = run_writer(shmptr, semid);
             if (rc != EXIT_SUCCESS)
             {
-                perror("Cannot write in shared memory");
+                perror("Writer error");
                 exit(1);
             }
             return EXIT_SUCCESS;
         }
     }
-    // Ждем завершения процессов
     for (size_t i = 0; i < CHILDREN_COUNT; ++i)
     {
         int status;
         if (waitpid(child_pids[i], &status, 0) == -1)
         {
-            perror("Waitpid error");
+            perror("waitpid error");
             exit(1);
         }
 
         if (WIFEXITED(status))
-            printf("Дочерний процесс (ID %d) завершил свою работу (код завершения: %d)\n", child_pids[i], WEXITSTATUS(status));
+            printf("Child process (PID %d) finished (code: %d)\n", child_pids[i], WEXITSTATUS(status));
         else if (WIFSIGNALED(status))
-            printf("Дочерний процесс (ID %d) завершился: получен сигнал %d\n", child_pids[i], WTERMSIG(status));
+            printf("Child process (PID %d) finished. Signal received: %d\n", child_pids[i], WTERMSIG(status));
         else if (WIFSTOPPED(status))
-            printf("Дочерний процесс (ID %d) остановился: получен сигнал %d\n", child_pids[i], WSTOPSIG(status));
+            printf("Child process (PID %d) stopped. Signal received %d\n", child_pids[i], WSTOPSIG(status));
     }
+    if (shmdt(shmptr) == -1)
+    {
+        perror("shmdt error");
+        exit(1);
+    }
+    if (shmctl(shmid, IPC_RMID, NULL) == -1)
+    {
+        perror("shmctl error");
+        exit(1);
+    }
+    if (semctl(semid, 0, IPC_RMID) == -1)
+    {
+        perror("semctl error");
+        exit(1);
+    }
+    return EXIT_SUCCESS;
 }
